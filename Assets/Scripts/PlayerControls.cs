9b87167516d9a02ea6bddae318a08480d93c6cfb
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-06 06:53:49 +0100

    Movement
    Added movement and clamped movement.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,9 @@ public class PlayerControls : MonoBehaviour
     [SerializeField]
     InputAction _movement;
 
+    [SerializeField]
+    float _controlsSpeed = 5f, _xRange = 5f, _yRange = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,10 +30,19 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = _movement.ReadValue<Vector2>().x;
-        float verticalThrow = _movement.ReadValue<Vector2>().y;
+        float xThrow = _movement.ReadValue<Vector2>().x;
+        float yThrow = _movement.ReadValue<Vector2>().y;
 
-        Debug.Log("H: " + horizontalThrow);
-        Debug.Log("V: " + verticalThrow);
+        float xOffset = xThrow * _controlsSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -_xRange, _xRange);
+
+        float yOffset = yThrow * _controlsSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -_yRange, _yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
+        
     }
 }
