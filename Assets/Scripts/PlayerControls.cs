5f8fee9aac842eacc844f83816d002a2827c92c4
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-06 06:03:20 +0100

    Controls
    Added movement using new input system

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField]
+    InputAction _movement;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable()
+    {
+        _movement.Enable(); 
+    }
+
+    private void OnDisable()
+    {
+        _movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalThrow = _movement.ReadValue<Vector2>().x;
+        float verticalThrow = _movement.ReadValue<Vector2>().y;
+
+        Debug.Log("H: " + horizontalThrow);
+        Debug.Log("V: " + verticalThrow);
     }
 }
