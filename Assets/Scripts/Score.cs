cf1b3af4d8f1d16aacb7076901284e29fd0efa60
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-08 10:16:12 +0100

    Score
    Added mechanic to display score on screen.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
@@ -8,12 +9,15 @@ public class Score : MonoBehaviour
 
     int _score;
 
+    [SerializeField] Text _scoreText;
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
+
     }
 
     // Start is called before the first frame update
@@ -25,11 +29,11 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void IncreaseScore(int amountToIncrease)
     {
         _score += amountToIncrease;
+        _scoreText.text = "Score: " + _score.ToString();
     }
 }
