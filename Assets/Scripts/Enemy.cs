81c0999ef16355caad14f4d7448380978eba081d
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-08 11:55:55 +0100

    Animation
    Reworked Script for hitPoints.
    
    Added Enemy Materials and animations.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,32 +5,43 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int _increaseScoreAmount = 10;
+    [SerializeField] int _hitPoints = 3;
 
     [SerializeField] ParticleSystem _explosionParticles;
+    [SerializeField] ParticleSystem _hitParticles;
+
+    Rigidbody _rb;
 
     MeshRenderer _meshRenderer;
 
     private void Awake()
     {
-        _meshRenderer = GetComponent<MeshRenderer>();
+        _rb = gameObject.AddComponent<Rigidbody>();
+        _rb.useGravity = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        DestroyEnemy();
+
+        if (_hitPoints < 1)
+        {
+            DestroyEnemy();
+        }
     }
 
     private void ProcessHit()
     {
+        _hitPoints--;
+        Debug.Log($"Hitpoints Left: {_hitPoints}");
         Score.instance.IncreaseScore(_increaseScoreAmount);
+        _hitParticles.Play();
     }
 
     private void DestroyEnemy()
     {
         _explosionParticles.Play();
-        _meshRenderer.enabled = false;
-        Destroy(gameObject, 1f);
+        Destroy(gameObject, 0.5f);
     }
 
     
--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -6,5 +6,7 @@ public class Tags : MonoBehaviour
 {
     public static string PLAYER_TAG = "Player";
     public static string ENEMY_TAG = "Enemy";
+    public static string SMALL_ENEMY_TAG = "Small Enemy";
+    public static string BIG_ENEMY_TAG = "Big Enemy";
     public static string OBSTACLE_TAG = "Obstacle";
 }
